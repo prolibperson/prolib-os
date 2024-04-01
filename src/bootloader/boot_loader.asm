4e10f9093026d804f409e4f1029cb3b1f8ed0b15
prolib <97302808+prolibperson@users.noreply.github.com>
2024-04-01 13:21:26 -0400

    Printing something to the screen!

--- src/bootloader/boot_loader.asm
+++ src/bootloader/boot_loader.asm
@@ -1,5 +1,21 @@
 [org 0x7c00] ; set origin
 
+;lets try to print something to the screen...
+mov ah, 0x0e    ; tty mode ofc
+mov al, 'H'     ; first letter!
+int 0x10        ; now lets use this interrupt to print it
+
+;same thing for here!
+mov ah, 0x0e
+mov al, 'i'
+int 0x10
+
+mov ah, 0x0e
+mov al, '!'
+int 0x10
+
+jmp $           ; now we hang!
+
 ;we love BIOS
-times 510 - ($-$$) db 0
+times 510 - ($-$$) db 0 ; lets fill the rest of the boot sector with zeros! (besides our code)
 dw 0xaa55
